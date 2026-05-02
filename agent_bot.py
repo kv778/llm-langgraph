f7c378bf7063cc00a56133cf32d4f78b5e3595d3
Kartik Vishnoi <kartikvishnoi778@gmail.com>
2026-05-02 16:59:54 +0530

    created an llm integration in langgraph

---
+++ agent_bot.py
@@ -0,0 +1,33 @@
+from typing import TypedDict, List
+from langchain_core.messages import HumanMessage
+from langchain_community.chat_models import ChatOllama
+from langgraph.graph import StateGraph, START, END
+from dotenv import load_dotenv
+
+import warnings
+warnings.filterwarnings("ignore")
+
+load_dotenv()
+llm = ChatOllama(model='gemma3:4b')
+
+class AgentState(TypedDict):
+    message: List[HumanMessage]
+
+def process(state: AgentState) -> AgentState:
+    response = llm.invoke(state['message'])
+    print(response.content)
+
+    return state
+
+graph = StateGraph(AgentState)
+graph.add_node("bot", process)
+
+graph.set_entry_point("bot")
+graph.set_finish_point("bot")
+
+agent = graph.compile()
+
+user_input = input("Enter message: ")
+while user_input != 'exit':
+    response = agent.invoke({'message': [HumanMessage(content=user_input)]})
+    user_input = input("Enter message: ")
